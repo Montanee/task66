3caa92bb9cbe3d59ef01fd2d14170f3ad1b9816b
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:45:07 +0300

    Добавьте файлы проекта.

---
+++ task6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите сумму вклада:");
+        decimal сумма = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Введите количество месяцев:");
+        int количество_месяцев = Convert.ToInt32(Console.ReadLine());
+
+        for (int месяц = 1; месяц <= количество_месяцев; месяц++)
+        {
+            сумма += сумма * 0.07m; // начисление 7% процентов
+        }
+
+        Console.WriteLine($"Итоговая сумма вклада после {количество_месяцев} месяцев: {сумма:C}");
+    }
+}
